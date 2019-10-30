19287fd47c23c4797deeb698eea857ebef8c212d
Sean Hellum <seanhellum45@gmail.com>
2019-10-30 13:02:58 +0000

    type res

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.use(express.static("static"))
-app.get('/', function(res) {
+app.get('/', function(res:any) {
     res.render("index.html")
 })
 app.listen(3000)
