2cb3eec7ed3d7c620704fd85c8342abeff100164
Sean Hellum <seanhellum45@gmail.com>
2019-10-30 12:51:25 +0000

    minor tweaks

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.use(express.static("static"))
-app.get('/', function(req, res) {
+app.get('/', function(res) {
     res.render("index.html")
 })
 app.listen(3000)
